d0cea3b3e899260482dcd34fea5d13cf66223943
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-06-15 14:25:10 +0200

    Ensure dataset is always converted
    Failed on a fresh clone

--- prepublish.js
+++ prepublish.js
@@ -9,20 +9,22 @@ var gzip = zlib.createGzip();
 var sourceFile = __dirname + '/code_postaux_v201410_corr.csv';
 var destFile = __dirname + '/codes-postaux.json.gz';
 
-// Override first line
-var COLUMNS_NAMES = ['codeInsee', 'nomCommune', 'codePostal', 'libelleAcheminement'];
+if (fs.existsSync(sourceFile)) {  // else, we're added as a dependency
+    // Override first line
+    var COLUMNS_NAMES = ['codeInsee', 'nomCommune', 'codePostal', 'libelleAcheminement'];
 
-// Read source file
-fs.createReadStream(sourceFile)
-    // Parse CSV as Object
-    .pipe(csvParse({ delimiter: ';', trim: true, columns: COLUMNS_NAMES }))
-    // Turn into JSON Array String
-    .pipe(JSONStream.stringify())
-    // Deflate
-    .pipe(gzip)
-    // Write gzipped json file
-    .pipe(fs.createWriteStream(destFile))
-    // Finished!
-    .on('finish', function () {
-        console.log('Dataset has been successfully converted and deflated');
-    });
+    // Read source file
+    fs.createReadStream(sourceFile)
+        // Parse CSV as Object
+        .pipe(csvParse({ delimiter: ';', trim: true, columns: COLUMNS_NAMES }))
+        // Turn into JSON Array String
+        .pipe(JSONStream.stringify())
+        // Deflate
+        .pipe(gzip)
+        // Write gzipped json file
+        .pipe(fs.createWriteStream(destFile))
+        // Finished!
+        .on('finish', function () {
+            console.log('Dataset has been successfully converted and deflated');
+        });
+}
