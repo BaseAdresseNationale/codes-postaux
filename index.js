8f059da2f9b31cc6d683255a7c3f63c797d20bec
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-06-15 12:32:34 +0200

    Add dead communes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var index = {};
+var data = require('./codes-postaux.json'),
+    index = {};
 
-require('./codes-postaux.json').forEach(function (entry) {
+data.forEach(function (entry) {
   if (!(entry.codePostal in index)) {
     index[entry.codePostal] = [];
   }
@@ -9,4 +10,22 @@ require('./codes-postaux.json').forEach(function (entry) {
 
 exports.find = function(postalCode) {
     return index[postalCode] || [];
+};
+
+
+/** Communes with 0 inhabitants (“communes mortes pour la France”).
+ * A sensible dataset to use for testing in any application that has a precondition where postal codes should map to a physical person.
+ */
+var deadCommunes;  // lazy load & memoize
+
+exports.dead = function getDeadCommunes() {
+    if (! deadCommunes) {
+        var DEAD_INSEE_CODES = [ '50173', '55039', '55050', '55239', '55307', '55139' ];
+
+        deadCommunes = data.filter(function(commune) {
+            return DEAD_INSEE_CODES.indexOf(commune.codeInsee) > -1;
+        });
+    }
+
+    return deadCommunes;
 };
--- test.js
+++ test.js
@@ -8,3 +8,13 @@ assert.equal(actual.codePostal, '75002');
 assert.equal(actual.nomCommune, 'PARIS 02');
 assert.equal(actual.codeInsee, '75102');
 assert.equal(actual.libelleAcheminement, 'PARIS');
+
+
+var actual = subject.dead();
+
+assert.ok(actual instanceof Array);
+assert.equal(actual.length, 6);
+assert.ok(actual[0] instanceof Object);
+assert.ok(actual[0].codeInsee.match(/^\d{5}$/));
+assert.ok(actual[0].codePostal.match(/^\d{5}$/));
+assert.ok(actual[0].nomCommune.match(/^[A-Z ]+$/));
