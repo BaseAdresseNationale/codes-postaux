b14f8bfc38c72ec65089e72678575ff9114a7d49
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-06-24 17:47:39 +0200

    Make dead communes pure documentation

--- test.js
+++ test.js
@@ -8,11 +8,3 @@ assert.equal(actual.codePostal, '75002');
 assert.equal(actual.nomCommune, 'PARIS 02');
 assert.equal(actual.codeInsee, '75102');
 assert.equal(actual.libelleAcheminement, 'PARIS');
-
-
-assert.ok(subject.dead instanceof Array);
-assert.equal(subject.dead.length, 6);
-assert.ok(subject.dead[0] instanceof Object);
-assert.ok(subject.dead[0].codeInsee.match(/^\d{5}$/));
-assert.ok(subject.dead[0].codePostal.match(/^\d{5}$/));
-assert.ok(subject.dead[0].nomCommune.match(/^[A-Z ]+$/));
