8fa0408ed24e4732a4f58f7a97ccc7e9c5f4775a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-03-09 11:53:33 +0100

    Fix full result

--- full.js
+++ full.js
@@ -39,7 +39,7 @@ function pariteOK(numero, codeParite) {
 }
 
 function formatResult(entry) {
-  return pick(entry, 'codePostal', 'libelleAdressage')
+  return pick(entry, 'codePostal', 'libelleAcheminement')
 }
 
 function findCodePostal(codeCommune, codeVoie, numero, repetition) {
