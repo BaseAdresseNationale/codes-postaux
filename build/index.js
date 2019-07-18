449ae5858ffdeaf0b6a4ea5cb9fe53468a986d00
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-07-19 00:39:18 +0200

    Implement default postcode

--- build/index.js
+++ build/index.js
@@ -23,9 +23,40 @@ function buildCompact(codesPostaux) {
     .value()
 }
 
+function expandWithDefault(codesPostaux) {
+  return chain(codesPostaux)
+    .groupBy('codeCommune')
+    .map((codesPostauxCommune, codeCommune) => {
+      if (!codesPostauxCommune.some(cp => cp.codeVoie === 'XXXX')) {
+        const codePostal = chain(codesPostauxCommune)
+          .countBy('codePostal')
+          .toPairs()
+          .maxBy(p => p[1])
+          .value()[0]
+
+        const libelleAcheminement = chain(codesPostauxCommune)
+          .countBy('libelleAcheminement')
+          .toPairs()
+          .maxBy(p => p[1])
+          .value()[0]
+
+        codesPostauxCommune.push({
+          codeCommune,
+          codePostal,
+          libelleAcheminement,
+          codeVoie: 'XXXX'
+        })
+      }
+
+      return codesPostauxCommune
+    })
+    .flatten()
+    .value()
+}
+
 async function main() {
   const buffer = await getLatestFIMOCTFileBuffer()
-  const codesPostaux = await extractFromFIMOCT(buffer)
+  const codesPostaux = expandWithDefault(await extractFromFIMOCT(buffer))
   codesPostaux.forEach(e => expandWithCommune(e))
   await writeAsJSONFile(join(__dirname, '..', 'codes-postaux-full.json'), codesPostaux)
   const codesPostauxCompact = buildCompact(codesPostaux)
